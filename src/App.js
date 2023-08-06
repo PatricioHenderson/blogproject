c29e755719dc91e3657a588d897425238071e704
patriciohv@getfairplay.com <patriciohv@getfairplay.com>
2023-08-06 14:09:13 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Posts from './components/Posts';
+
+
+function App() {
+  return (
+    <div className="Blog Project">
+      {/* <header className="App-header">
+      </header> */}
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
